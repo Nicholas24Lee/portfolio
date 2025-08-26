0af48c33518478a55746c8c664af997704f6745a
Nick <nickylee182005@gmail.com>
2025-08-26 16:38:11 +0800

    Reformat home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
+import './App.css'
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className="app-container">
+        <Navbar />
+        <div className="routes-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,7 @@ import './Home.css'
 
 export default function Home() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center", // horizontal center
-        alignItems: "center",     // vertical center
-        width: "100vw",           // full viewport width
-        height: "100vh",          // full viewport height
-        margin: 0,
-        padding: 0,
-      }}
-    >
+    <div className="home-container">
       <h1 className="home-title">Nicholas</h1>
     </div>
   );
