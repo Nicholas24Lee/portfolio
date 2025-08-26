da56a1177aa03771d309f786969fa1f5b9c507c4
Nick <nickylee182005@gmail.com>
2025-08-26 16:54:20 +0800

    Reformat all pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,7 @@
 export default function Contact() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center", // horizontal center
-        alignItems: "center",     // vertical center
-        width: "100vw",           // full viewport width
-        height: "100vh",          // full viewport height
-        margin: 0,
-        padding: 0,
-      }}
-    >
-      <h1>Get in touch via email or social media.</h1>
+    <div className="page-container">
+      <h1 style={{ textAlign: "center" }}>Get in touch via email or social media.</h1>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import './Home.css'
 
 export default function Home() {
   return (
-    <div className="home-container">
+    <div className="page-container">
       <h1 className="home-title">Nicholas</h1>
     </div>
   );
